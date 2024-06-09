3c4a0218de342c31cf088dcd41aab7a82a180be3
Ayham <ayham.2lloush@gmail.com>
2024-06-09 14:27:11 -0500

    added database folder
    added database folder and inside it i created file named 'db.php' , created database and table for users, it's empty now and didn't connect the form with it yey

---
+++ database/db.php
@@ -0,0 +1,44 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+
+	$conn = mysqli_connect($servername, $username, $password);
+
+	if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+	}
+ 
+  $createDatabaseQuery= "CREATE DATABASE IF NOT EXISTS mydb" ;
+  if(mysqli_query( $conn , $createDatabaseQuery )){
+    echo "Database created successfully <br>" ;
+  }
+  else{
+    echo "Error creating database :" . mysqli_error($conn) ;
+  }
+
+  mysqli_select_db($conn,"mydb") ;
+
+  $createUsersTableQuery= "
+    CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone_number VARCHAR(255),
+    clinic_name VARCHAR(255),
+    user_name VARCHAR(255),
+    password VARCHAR(255),
+    full_address VARCHAR(255),
+    gender VARCHAR(255)
+  ); " ;
+
+  if(mysqli_query( $conn , $createUsersTableQuery )){
+    echo "Users table created successfully" ;
+  }
+  else{
+    echo "Error creating users table :" . mysqli_error($conn) ;
+  }
+
+  mysqli_close($conn) ;
+?>
