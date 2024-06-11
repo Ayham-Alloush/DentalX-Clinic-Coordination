e15d32e64c18382679a1bb325c9e5c782e1be0d4
Ayham <ayham.2lloush@gmail.com>
2024-06-10 21:05:37 -0500

    edited main.js for doc-signup
    added eventListener (submit) to prevent submit if any input is invalid , and to add class was-valid to the form

--- signup/js/main.js
+++ signup/js/main.js
@@ -1,3 +1,21 @@
+"use strict" ;
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
+
+
 function validateForm(){
   var checkboxes = document.querySelectorAll('input[type="radio"]');
   var checked = false;
