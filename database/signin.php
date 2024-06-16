25d2081940b59e262d2155e5ef2a97eb01e7027c
Ayham <ayham.2lloush@gmail.com>
2024-06-16 11:59:37 -0500

    updated signin.php
    added section which will handle Prosthodontist signin

--- database/signin.php
+++ database/signin.php
@@ -40,7 +40,34 @@
         header('location: ../login_signup/index.html?x=2') ;
     }
   }
+  // if Prosthodontist is signing in ($title=='Prosthodontist')
   else{
-    // here i have to add query to a new table named lab_users .
+    $query = "SELECT password FROM lab_users WHERE user_name = ?";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "s", $userName);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    if (mysqli_stmt_num_rows($stmt) == 1) {
+        mysqli_stmt_bind_result($stmt, $hashedPassword);
+        mysqli_stmt_fetch($stmt);
+
+        if (password_verify($password, $hashedPassword)) {
+            // Passwords match, authentication successful
+            header('Location: ../lab_home/index.php');
+            mysqli_stmt_close($stmt);
+            exit();
+        } else {
+            // Passwords don't match, authentication failed
+            // send the user to signin page again but with x=1
+            // then we use js to handle this .
+            header('location: ../login_signup/index.html?x=1') ;
+        }
+    } 
+    else {
+        // No matching username found
+        // send the user to signin page again but with x=1
+        // then we use js to handle this .
+        header('location: ../login_signup/index.html?x=2') ;
+    }
   }
 ?>
