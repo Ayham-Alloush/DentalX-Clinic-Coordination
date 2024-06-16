04473ccf5c4e3aa9e3fc7fbcf23d35577daf53e3
Ayham <ayham.2lloush@gmail.com>
2024-06-16 10:05:50 -0500

    fixed invalid feedback
    in js i removed function responible of validating radio buttons if one of them is checked , in index.html i just added required to all radio buttons , and moved (invalid feedback) section to the bottom of the last radio button

--- login_signup/js/main.js
+++ login_signup/js/main.js
@@ -15,29 +15,6 @@
     }, false)
   })
 
-
-
-
-function validateForm(){
-  var checkboxes = document.querySelectorAll('input[type="radio"]');
-  var checked = false;
-  var feed = document.getElementById('options') ;
-  
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checked = true;
-      break;
-    }
-  }
-  
-  if (!checked) {
-    feed.classList.add('d-block');
-    return false;
-  }
-  
-  return true;
-}
-
 // Get the URL parameters
 const urlParams = new URLSearchParams(window.location.search);
 
--- signup/js/main.js
+++ signup/js/main.js
@@ -14,24 +14,3 @@ Array.from(forms).forEach(form => {
     form.classList.add('was-validated')
   }, false)
 })
-
-
-function validateForm(){
-  var checkboxes = document.querySelectorAll('input[type="radio"]');
-  var checked = false;
-  var feed = document.getElementById('options') ;
-  
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checked = true;
-      break;
-    }
-  }
-  
-  if (!checked) {
-    feed.classList.add('d-block');
-    return false;
-  }
-  
-  return true;
-}
