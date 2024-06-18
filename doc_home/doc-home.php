2f638a0489b88124a597b03d8a0d9d931a6451a5
Ayham <ayham.2lloush@gmail.com>
2024-06-18 10:17:37 -0500

    updated doc-home file
    chenged (about) href from # to about.html

--- doc_home/doc-home.php
+++ doc_home/doc-home.php
@@ -57,7 +57,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="../about/about.html">
                 <i class="fa-solid fa-circle-question"></i>
                 حول
               </a>
@@ -106,7 +106,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="../about/about.html">
                   <i class="fa-solid fa-circle-question"></i>
                   حول
                 </a>
