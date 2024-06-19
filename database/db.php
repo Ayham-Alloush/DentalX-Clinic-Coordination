0b615c8ec8b25e9f608b4dfa3256716161eeff93
Ayham <ayham.2lloush@gmail.com>
2024-06-19 11:23:09 -0500

    created orders table
    created orders table , and created order-details table which will be just for test , in the future we will delete order-details table or we will alter it

--- database/db.php
+++ database/db.php
@@ -55,10 +55,52 @@
   ); " ;
 
   if(mysqli_query( $conn , $createLabUsersTableQuery )){
-    echo "lab_Users table created successfully" ;
+    echo "lab_Users table created successfully <br>" ;
   }
   else{
     echo "Error creating users table :" . mysqli_error($conn) ;
+  }
+
+  // this is table for testing , it will be deleted or altered .
+  $createOrderDetailsTableQuery= "
+  CREATE TABLE IF NOT EXISTS order_details (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  order_details VARCHAR(255)
+  ); " ;
+
+  if(mysqli_query( $conn , $createOrderDetailsTableQuery )){
+    echo "order_details table created successfully <br>" ;
+  }
+  else{
+    echo "Error creating order_details table :" . mysqli_error($conn) ;
+  }
+
+  $createOrdersTableQuery= "
+  CREATE TABLE IF NOT EXISTS orders (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  doc_id INT,
+  lab_id INT,
+  clinic_name VARCHAR(255),
+  lab_name VARCHAR(255),
+  doc_name VARCHAR(255),
+  prosthodontist_name VARCHAR(255),
+  lab_type VARCHAR(255),
+  order_date DATE,
+  receive_date DATE,
+  patient_name VARCHAR(255),
+  price DECIMAL(10, 2),
+  status VARCHAR(255),
+  order_details_id INT,
+  FOREIGN KEY (doc_id) REFERENCES users(id),
+  FOREIGN KEY (lab_id) REFERENCES lab_users(id),
+  FOREIGN KEY (order_details_id) REFERENCES order_details(id)
+); " ;
+
+  if(mysqli_query( $conn , $createOrdersTableQuery )){
+    echo "Orders table created successfully <br>" ;
+  }
+  else{
+    echo "Error creating orders table :" . mysqli_error($conn) ;
   }
 
   mysqli_close($conn) ;
